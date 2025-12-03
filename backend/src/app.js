45e948dcf9956f192d76e698be3f37e8e4d4d9f8
Estrella Ticona <u202210659@upc.edu.pe>
2025-12-03 17:22:05 -0500

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -33,7 +33,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'x-api-key']
 }));
-app.use(cors())
+/* app.use(cors()) */
 
 // Aumentando limite a 50mb
 app.use(express.json({
